72e4fc1523441781322019a104b362adab444342
progchris00 <prog.christian07@gmail.com>
2023-06-29 15:31:36 +0800

    Enabled score restarting

--- Pygames/Runner/main.py
+++ Pygames/Runner/main.py
@@ -2,32 +2,33 @@ import pygame
 from sys import exit
 
 def display_score():
-    current_time = round(pygame.time.get_ticks() / 1000)
+    current_time = round(pygame.time.get_ticks() / 1000) - start_time
     score_surf = test_font.render(f'{current_time}', False, (64, 64, 64))
     score_rect = score_surf.get_rect(center = (400, 50))
     screen.blit(score_surf, score_rect)
 
-pygame.init()
 
+pygame.init()
 screen = pygame.display.set_mode((800, 400), pygame.RESIZABLE)
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font("font/Pixeltype.ttf", 100)
 end_font = pygame.font.Font("font/Pixeltype.ttf", 20)
 game_active = True
+start_time = 0
+
 
 sky_surface = pygame.image.load('graphics/Sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
 
+
 snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 snail_rect = snail_surf.get_rect(midbottom = (600, 300))
+
 
 player_surf = pygame.image.load('graphics/Player/player_walk_1.png').convert_alpha()
 player_rect = player_surf.get_rect(midbottom = (80, 300))
 player_gravity = 0
-
-# score_surf = test_font.render('Ang Pogi ko', False, (64, 64, 64))
-# score_rect = score_surf.get_rect(center = (400, 90))
 
 while True:
     for event in pygame.event.get():
@@ -43,11 +44,10 @@ while True:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
                 snail_rect.left = 800
+                start_time = round(pygame.time.get_ticks() / 1000)
 
     if game_active:       
         screen.blits([(sky_surface, (0, 0)), (ground_surface, (0, 300))])
-        # pygame.draw.rect(screen, '#c0e8ec', score_rect)
-        # screen.blit(score_surf, score_rect)
         display_score()
 
         snail_rect.left -= 6
@@ -63,8 +63,6 @@ while True:
         # Collision
         if snail_rect.colliderect(player_rect):
             game_active = False
-    # else:
-    #     screen.fill("Yellow")
-
+            
     pygame.display.update()
     clock.tick(60)
