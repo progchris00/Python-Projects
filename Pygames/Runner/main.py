8c98a7a9e99c22188cf475140fef10c9eb3c1e2b
progchris00 <prog.christian07@gmail.com>
2023-07-04 19:11:17 +0800

    Added obstacle sprite class

--- Pygames/Runner/main.py
+++ Pygames/Runner/main.py
@@ -39,6 +39,26 @@ class Player(pygame.sprite.Sprite):
         self.apply_gravity()
         self.animation_state()
 
+class Obstacle(pygame.sprite.Sprite):
+    def __init__(self, type):
+        super().__init__()
+        
+        if type == 'fly':
+            fly_frame1 = pygame.image.load('graphics/Fly/Fly1.png').convert_alpha()
+            fly_frame2 = pygame.image.load('graphics/Fly/Fly2.png').convert_alpha()
+            self.frames = [fly_frame1, fly_frame2]
+            y_pos = 210
+
+        else:
+            snail_frame_1 = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+            snail_frame_2 = pygame.image.load('graphics/snail/snail2.png').convert_alpha()
+            self.frames = [snail_frame_1, snail_frame_2]
+            y_pos = 300
+
+        self.animation_index = 0
+        self.image = self.frames[self.animation_index]
+        self.rect = self.image.get_rect(midbottom = (randint(900, 1100), y_pos))
+
 def display_score():
     current_time = round(pygame.time.get_ticks() / 1000) - start_time
     score_surf = test_font.render(f'Score: {current_time}', False, (64, 64, 64))
