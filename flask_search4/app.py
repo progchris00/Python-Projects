da0fe4d3fb40a4724e612f6e8af98cca62a53e24
progchris00 <prog.christian07@gmail.com>
2023-07-08 13:33:09 +0800

    Implement data passing

--- flask_search4/app.py
+++ flask_search4/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from vsearch import search4letters
 
 app = Flask(__name__)
@@ -7,12 +7,21 @@ app = Flask(__name__)
 def hello() -> str:
     return 'Hello world from Flask!'
 
-@app.route('/search4.', methods=['POST'])
+@app.route('/search4', methods=['POST'])
 def do_search() -> str:
-    return str(search4letters('life, the universe, and everything!', 'eiru'))
+    return str(search4letters(request.form['phrase'], request.form['letters']))
 
 @app.route('/entry')
 def entry_page() -> 'html':
-    return render_template('entry.html', the_title='Welcome to search4letters on the web!')
+    return render_template('entry.html', 
+                           the_title='Welcome to search4letters on the web!')
+
+# @app.route('/result')
+# def entry_page() -> 'html':
+#     return render_template('result.html', 
+#                            the_title='Welcome to search4letters on the web!',
+#                            the_phrase=phrase
+#                            the_letters=letters
+#                            the_result=)
 
 app.run(debug=True)
