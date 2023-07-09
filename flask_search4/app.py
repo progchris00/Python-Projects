21ef1ad4476661c15cf4b81b800663e42fdb2420
progchris00 <prog.christian07@gmail.com>
2023-07-09 18:25:11 +0800

    Debugged code

--- flask_search4/app.py
+++ flask_search4/app.py
@@ -8,18 +8,21 @@ def hello() -> str:
     return 'Hello world from Flask!'
 
 @app.route('/search4', methods=['POST'])
-def do_search() -> str:
-    return str(search4letters(request.form['phrase'], request.form['letters']))
+def do_search() -> 'html':
+    phrase = request.form['phrase']
+    letters = request.form['letters']
+    title = 'Here are your results'
+    results = str(search4letters(phrase, letters))
+    return render_template('results.html',
+               the_letters = letters,
+               the_title = title,
+               the_results = results,
+               the_phrase = phrase)
 
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html', 
                            the_title='Welcome to search4letters on the web!')
 
-@app.route('/results')
-def result_page() -> 'html':
-    return render_template('results.html', 
-                           the_title='Welcome to search4letters on the web!',
-                           the_phrase=request.form['phrase'])
 
 app.run(debug=True)
