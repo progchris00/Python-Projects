69e7c1fca7bdaf4e4329632e16ddbf5bdc3f2a17
progchris00 <prog.christian07@gmail.com>
2023-07-10 07:42:04 +0800

    Associated two urls in one function and removed redirection

--- flask_search4/app.py
+++ flask_search4/app.py
@@ -1,11 +1,8 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request
 from vsearch import search4letters
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello() -> '302':
-    return redirect('/entry')
 
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
@@ -19,6 +16,7 @@ def do_search() -> 'html':
                the_results = results,
                the_phrase = phrase)
 
+@app.route('/')
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html', 
