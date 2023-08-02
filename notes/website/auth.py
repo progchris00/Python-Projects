c18134e0c2e79c4aa11cc71bbd58bf19dbb7c424
progchris00 <prog.christian07@gmail.com>
2023-08-03 07:04:10 +0800

    Implement variable passing

--- notes/website/auth.py
+++ notes/website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
@@ -13,4 +13,10 @@ def signup_page() -> 'html':
 
 @auth.route('/home', methods=['POST'])
 def home_page() -> 'html':
-    return render_template('home.html')
+    name = request.form['firstname']
+    email = request.form['email']
+    password = request.form['password1']
+    return render_template('home.html',
+                           the_name=name,
+                           the_email=email,
+                           the_password=password)
