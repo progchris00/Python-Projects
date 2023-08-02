22ca6a394d6a2de68b0650ebb21562ec8f0bc3fe
progchris00 <prog.christian07@gmail.com>
2023-08-02 19:47:55 +0800

    Added forms

--- notes/website/auth.py
+++ notes/website/auth.py
@@ -4,9 +4,13 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login_page() -> 'html':
-    return "<h2> Login </h2> "
+    return render_template('login.html')
 
 @auth.route('/sign-up')
 def signup_page() -> 'html':
     return render_template('signup.html',
-                            the_title = "Sign Up")
+                            the_title = "Testing")
+
+@auth.route('/home', methods=['POST'])
+def home_page() -> 'html':
+    return render_template('home.html')
