bdbf92483753c7214fe52a6d2da9074a20d93c1a
progchris00 <prog.christian07@gmail.com>
2023-08-07 17:32:23 +0800

    Implemented user creation function

--- notes/website/auth.py
+++ notes/website/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 auth = Blueprint('auth', __name__)
 
@@ -7,7 +10,7 @@ def login_page():
     return render_template('login.html')
 
 @auth.route('/signup', methods=['Get', 'POST'])
-def home_page():
+def sign_up():
     if request.method == 'POST':
         name = request.form.get('firstname')
         email = request.form.get('email')
@@ -21,6 +24,11 @@ def home_page():
         if password1 != password2:
             flash("Passwords do not match", category='error')
         else:
+            new_user = User(email=email, first_name=name, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash("Account succesfully created!")
+            return redirect(url_for('views.home'))
+
 
     return render_template('signup.html')
