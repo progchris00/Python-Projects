a4722ae8e9e2e691230c23524dd6fcfb03b1b73a
progchris00 <prog.christian07@gmail.com>
2023-08-07 09:39:25 +0800

    Fixed database creation

--- notes/website/__init__.py
+++ notes/website/__init__.py
@@ -19,12 +19,8 @@ def create_app():
 
     from .models import User, Note
 
-    create_database(app)
+    with app.app_context():
+        db.create_all()
 
     return app
 
-
-def create_database(app):
-    if not path.exists('website/' + DB_NAME):
-        db.create_all(app=app)
-        print('Created Database!')
