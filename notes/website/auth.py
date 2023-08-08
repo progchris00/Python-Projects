4e8604ed616b0324471d8e255c0106e042daf992
progchris00 <prog.christian07@gmail.com>
2023-08-08 15:45:38 +0800

    Added condition on navbar

--- notes/website/auth.py
+++ notes/website/auth.py
@@ -2,6 +2,8 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
+
 
 auth = Blueprint('auth', __name__)
 
@@ -15,13 +17,22 @@ def login_page():
         if user: 
             if check_password_hash(user.password, password):
                 flash("Logged in successfully!", category='success')
+                login_user(user, remember=True)
                 return redirect(url_for('views.home'))
             else:
                 flash("Incorrect password", category='error')
         else:
             flash("Email does not exists", category='error')
 
-    return render_template('login.html')
+    return render_template('login.html', user=current_user)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login_page'))
+
 
 @auth.route('/signup', methods=['Get', 'POST'])
 def sign_up():
@@ -45,8 +56,9 @@ def sign_up():
             new_user = User(email=email, first_name=name, password=generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
+            login_user(user, remember=True)
             flash("Account succesfully created!")
             return redirect(url_for('views.home'))
 
 
-    return render_template('signup.html')
+    return render_template('signup.html', user=current_user)
