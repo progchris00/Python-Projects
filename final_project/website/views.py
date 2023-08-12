c4a2b50ea2c5fe2e78979e33e5a86a6f250a0c26
progchris00 <prog.christian07@gmail.com>
2023-08-12 16:18:18 +0800

    Moved functions from ortiz.py to __init__.py

--- final_project/website/views.py
+++ final_project/website/views.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template, request, flash, jsonify
-
+from flask import Blueprint, render_template, request, flash, redirect, url_for, session
+from . import *
 
 views = Blueprint('views', __name__)
 
@@ -7,9 +7,13 @@ views = Blueprint('views', __name__)
 def home():
     return render_template('home.html')
 
-
-@views.route('/einstein')
+@views.route('/einstein', methods=['GET','POST'])
 def einstein():
+    if request.method == 'POST':
+        mass = request.form.get('mass')
+        result = cal_energy(int(mass))
+        return render_template('result_eins.html', mass=mass, result=result)
+
     return render_template('einstein.html')
 
 @views.route('/figlet')
@@ -23,3 +27,4 @@ def meal():
 @views.route('/shirtificate')
 def shirt():
     return render_template('shirt.html')
+
