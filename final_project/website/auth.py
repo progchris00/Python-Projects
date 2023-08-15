45b32b5e7ae5c74f2ee793664bf3e4aa06952e87
progchris00 <prog.christian07@gmail.com>
2023-08-16 07:26:11 +0800

    Added comments and remove code to practice

--- final_project/website/auth.py
+++ final_project/website/auth.py
@@ -44,29 +44,11 @@ def logout():
 
 @auth.route('/signup', methods=['Get', 'POST'])
 def sign_up():
-    if request.method == 'POST':
-        name = request.form.get('firstname')
-        email = request.form.get('email')
-        password1 = request.form.get('password1')
-        password2 = request.form.get('password2')
-
-        user = User.query.filter_by(email=email).first()
-        if user:
-            flash('Email already exists', category='error')
-
-        elif len(email.split('@')[0]) < 3:
-            flash("Username must be 3 characters long", category='error')
-        elif len(name) < 3:
-            flash("Name must be 3 characters long.", category='error')
-        elif password1 != password2:
-            flash("Passwords do not match", category='error')
-        else:
-            new_user = User(email=email, first_name=name, password=generate_password_hash(password1, method='sha256'))
-            db.session.add(new_user)
-            db.session.commit()
-            login_user(user, remember=True)
-            flash("Account succesfully created!")
-            return redirect(url_for('views.home'))
-
-
-    return render_template('signup.html', user=current_user)
+#if method is post get required attributes
+#make a user instance then filter it
+#check if there is already an account associated with that email, flash message
+#checks the input for len, name length and if password 1 and 2 match
+#if okay, create a new user with the required attributes except for the password, generate a password hash for that one with method='sha256'
+#add that new_user as a db session and commit that session
+#then login that user and redirect to views.home
+# if method is get and not post, render signup template
