1aa9e5477300f4f77b173b43ac3a4ad86fc61b47
progchris00 <prog.christian07@gmail.com>
2023-08-27 11:19:53 +0800

    Moved flaskr folder

--- notes/website/auth.py
+++ notes/website/auth.py
@@ -34,7 +34,7 @@ def logout():
     return redirect(url_for('auth.login_page'))
 
 
-@auth.route('/signup', methods=['Get', 'POST'])
+@auth.route('/signup', methods=['GET', 'POST'])
 def sign_up():
     if request.method == 'POST':
         name = request.form.get('firstname')
@@ -56,7 +56,7 @@ def sign_up():
             new_user = User(email=email, first_name=name, password=generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
-            login_user(user, remember=True)
+            login_user(new_user, remember=True)
             flash("Account succesfully created!")
             return redirect(url_for('views.home'))
 
