06aebd62858f0d648a1ffbae45393d94221a006e
progchris00 <prog.christian07@gmail.com>
2023-08-28 12:54:06 +0800

    Fixed typographic errors

--- flask-tutorial/flaskr/auth.py
+++ flask-tutorial/flaskr/auth.py
@@ -62,7 +62,7 @@ def login():
         
         flash(error)
 
-    return render_template('auth/login/html')
+    return render_template('auth/login.html')
 
 @bp.before_app_request
 def load_logged_in_user():
