d7a3725dc0730e6712d562d3798b0cf2747a4cc8
progchris00 <prog.christian07@gmail.com>
2023-08-29 13:26:19 +0800

    Created index.html

--- flask-tutorial/flaskr/blog.py
+++ flask-tutorial/flaskr/blog.py
@@ -14,7 +14,7 @@ def index():
     db = get_db()
     posts = db.execute(
         'SELECT p.id, title, body, created, author_id, username'
-        'FROM post p JOIN user u ON p.author_id = u.id'
-        'ORDER BY created DESC'
-    ).fetchall
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' ORDER BY created DESC'
+    ).fetchall()
     return render_template('blog/index.html', posts=posts)
